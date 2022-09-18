71b86f420ed645b2dabe198a5fcfb9f5212bacb6
Максим Куба <maxkuba@Mac-mini-Maksim.local>
2022-09-18 16:23:37 +0300

    footer fix

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,12 +2,12 @@
     <div class="wrapper">
         <div class="bl-3w desktop">
             <a>
-                <picture><source srcset="view/images/logo_footer.webp" type="image/webp"><img src="view/images/logo_footer.png" width="192" height="161" alt="logo"></picture>
+                <picture><source srcset="{{asset('view/images/logo_footer.webp')}}" type="image/webp"><img src="{{asset('view/images/logo_footer.png')}}" width="192" height="161" alt="logo"></picture>
             </a>
         </div>
         <div class="bl-3w mob" style="padding: 13% 25px 3% 20px;flex-direction: row;">
             <a>
-                <picture><source srcset="view/images/logo_footer.webp" type="image/webp"><img src="view/images/logo_footer.png" width="68" height="57" alt="logo"></picture>
+                <picture><source srcset="{{asset('view/images/logo_footer.webp')}}" type="image/webp"><img src="{{asset('view/images/logo_footer.png')}}" width="68" height="57" alt="logo"></picture>
             </a>
             <p style="color: #FA9C15;padding-left:27px;">CustomVideo Streaming <br>and WebRTC development</p>
         </div>
@@ -16,49 +16,49 @@
                 <span class="drop-x h4">Services</span>
                 <svg xmlns="http://www.w3.org/2000/svg" class="drop-tr drop-x" width="22.276" height="12" viewBox="0 0 22.276 12"><defs><style>.a{fill:#fff;}</style></defs><g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)"/></g></g></svg>
                 <ul>
-                    <li><a href="web-development.html"><img alt="computer" src="view/images/computer.svg" height="16" style="padding-right:12px;">Web development</a></li>
-                    <li><a href="mobile-development.html"><img alt="mobile development" src="view/images/software-development.svg" height="16" style="padding-right:17px;">Mob development</a></li>
-                    <li><a href="/software-development.html"><img alt="setting" src="view/images/setting.svg" height="16" style="padding-right:15px;">Web Development kit</a></li>
-                    <li><a href="ui-ux.html"><img alt="art-and-design" src="view/images/art-and-design.svg" height="16" style="padding-right:15px;">UX/UI</a></li>
-                    <li><a href="qa.html"><img alt="qa" src="view/images/virus-warning(1).svg" height="16" style="padding-right:15px;">QA</a></li>
-                    <li><a href="rd.html"><img alt="rd" src="view/images/search(2).svg" height="16" style="padding-right:15px;">R&D </a></li>
+                    <li><a href="/web-development.html"><img alt="computer" src="{{asset('view/images/computer.svg')}}" height="16" style="padding-right:12px;">Web development</a></li>
+                    <li><a href="/mobile-development.html"><img alt="mobile development" src="{{asset('view/images/software-development.svg')}}" height="16" style="padding-right:17px;">Mob development</a></li>
+                    <li><a href="/software-development.html"><img alt="setting" src="{{asset('view/images/setting.svg')}}" height="16" style="padding-right:15px;">Web Development kit</a></li>
+                    <li><a href="/ui-ux.html"><img alt="art-and-design" src="{{asset('view/images/art-and-design.svg')}}" height="16" style="padding-right:15px;">UX/UI</a></li>
+                    <li><a href="/qa.html"><img alt="qa" src="{{asset('view/images/virus-warning(1).svg')}}" height="16" style="padding-right:15px;">QA</a></li>
+                    <li><a href="/rd.html"><img alt="rd" src="{{asset('view/images/search(2).svg')}}" height="16" style="padding-right:15px;">R&D </a></li>
                 </ul>
             </div>
 
             <div class="f-m-ul">
-                <a href="portfolio.html" class="h4">Case Studies</a>
+                <a href="/portfolio.html" class="h4">Case Studies</a>
             </div>
             <div class="f-m-ul drop-m3">
                 <span class="drop-x3 h4">Industries</span>
                 <svg xmlns="http://www.w3.org/2000/svg" class="drop-tr drop-x3" width="22.276" height="12" viewBox="0 0 22.276 12"><defs><style>.a{fill:#fff;}</style></defs><g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)"/></g></g></svg>
                 <ul>
-                    <li><a class="menuItem" href="/industries/webrtc-development.html"><img alt="webrtc development" src="view/images/computer.svg" height="16" style="padding-right:12px;">WebRTC</a></li>
-                    <li><a href="/industries/telemedicine-app-development.html"><img alt="telemedicine app development" src="view/images/software-development.svg" height="16" style="padding-right:17px;">Telemedicine App Development</a></li>
-                    <li><a href="/industries/fitness-app-development.html"><img alt="fitness app development" src="view/images/setting.svg" height="16" style="padding-right:15px;">Fitness App Development</a></li>
-                    <li><a href="/industries/dating-app-development.html"><img alt="dating app development" src="view/images/art-and-design.svg" height="16" style="padding-right:15px;">Dating App Development</a></li>
-                    <li><a href="/industries/video-chat-app-development.html"><img alt="video chat app" src="view/images/virus-warning(1).svg" height="16" style="padding-right:15px;">Video Chat App Development</a></li>
+                    <li><a class="menuItem" href="/industries/webrtc-development.html"><img alt="webrtc development" src="{{asset('view/images/computer.svg')}}" height="16" style="padding-right:12px;">WebRTC</a></li>
+                    <li><a href="/industries/telemedicine-app-development.html"><img alt="telemedicine app development" src="{{asset('view/images/software-development.svg')}}" height="16" style="padding-right:17px;">Telemedicine App Development</a></li>
+                    <li><a href="/industries/fitness-app-development.html"><img alt="fitness app development" src="{{asset('view/images/setting.svg')}}" height="16" style="padding-right:15px;">Fitness App Development</a></li>
+                    <li><a href="/industries/dating-app-development.html"><img alt="dating app development" src="{{asset('view/images/art-and-design.svg')}}" height="16" style="padding-right:15px;">Dating App Development</a></li>
+                    <li><a href="/industries/video-chat-app-development.html"><img alt="video chat app" src="{{asset('view/images/virus-warning(1).svg')}}" height="16" style="padding-right:15px;">Video Chat App Development</a></li>
                 </ul>
             </div>
             <div class="f-m-ul drop-m2">
                 <span class="drop-x2 h4">Solution</span>
                 <svg xmlns="http://www.w3.org/2000/svg" class="drop-tr drop-x2" width="22.276" height="12" viewBox="0 0 22.276 12"><defs><style>.a{fill:#fff;}</style></defs><g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)"/></g></g></svg>
                 <ul>
-                    <li><a href="vatra.html"><picture><source srcset="view/images/LOGOVatra.webp" type="image/webp"><img alt="vatra logo" src="view/images/LOGOVatra.png" height="16" style="padding-right:12px;"></picture></a></li>
-                    <li><a href="stusan.html"><img alt="stusan logo" src="view/images/Stusanlogo.svg" height="16" style="padding-right:17px;"></a></li>
+                    <li><a href="/vatra.html"><picture><source srcset="{{asset('view/images/LOGOVatra.webp')}}" type="image/webp"><img alt="vatra logo" src="{{asset('view/images/LOGOVatra.png')}}" height="16" style="padding-right:12px;"></picture></a></li>
+                    <li><a href="/stusan.html"><img alt="stusan logo" src="{{asset('view/images/Stusanlogo.svg')}}" height="16" style="padding-right:17px;"></a></li>
 
                 </ul>
             </div>
             <div class="f-m-ul">
-                <a href="about.html" class="h4">About us</a>
+                <a href="/about.html" class="h4">About us</a>
             </div>
             <div class="f-m-ul" style="border-bottom: 1px solid white;">
-                <a href="contacts.html" class="h4">Contacts us</a>
+                <a href="/contacts.html" class="h4">Contacts us</a>
             </div>
             <div class="block-3c space">
                 <div>
                     <div class="flex column" style="flex-direction: row; padding-top: 20px; padding-left: 25px;">
                         <script src="https://widget.clutch.co/static/js/widget.js?_v=20220716221822"></script> <div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="2" data-height="45"  data-darkbg="1" data-clutchcompany-id="114502" style="width: 193px;"></div>
-                        <a target="_blank" href="https://www.red5pro.com/"><picture><source srcset="view/images/red5_pro_logo.webp" type="image/webp"><img src="view/images/red5_pro_logo.png" width="147" height="48" alt="red5"></picture></a>
+                        <a target="_blank" href="https://www.red5pro.com/"><picture><source srcset="{{asset('view/images/red5_pro_logo.webp')}}" type="image/webp"><img src="{{asset('view/images/red5_pro_logo.png')}}" width="147" height="48" alt="red5"></picture></a>
                     </div>
                 </div>
                 <div>
@@ -72,25 +72,25 @@
                 <div>
                     <span class="h4">Services</span>
                     <ul>
-                        <li><a href="web-development.html">Web Development</a></li>
-                        <li><a href="mobile-development.html">Mobile Development</a></li>
-                        <li><a href="software-development.html">Software Development</a></li>
-                        <li><a href="ui-ux.html">UI/UX</a></li>
-                        <li><a href="qa.html">QA</a></li>
-                        <li><a href="rd.html">R&D </a></li>
+                        <li><a href="/web-development.html">Web Development</a></li>
+                        <li><a href="/mobile-development.html">Mobile Development</a></li>
+                        <li><a href="/software-development.html">Software Development</a></li>
+                        <li><a href="/ui-ux.html">UI/UX</a></li>
+                        <li><a href="/qa.html">QA</a></li>
+                        <li><a href="/rd.html">R&D </a></li>
                     </ul>
                 </div>
                 <div>
                     <span class="h4" style="padding-top: 22px;">Portfolio</span>
                     <ul>
-                        <li><a href="portfolio.html">Case Studies</a></li>
+                        <li><a href="/portfolio.html">Case Studies</a></li>
                     </ul>
                 </div>
                 <div>
                     <span  class="h4" style="padding-top: 22px;">Solution</span>
                     <ul>
-                        <li><a href="vatra.html">Vatra</a></li>
-                        <li><a href="stusan.html">Stusan</a></li>
+                        <li><a href="/vatra.html">Vatra</a></li>
+                        <li><a href="/stusan.html">Stusan</a></li>
                     </ul>
                 </div>
             </div>
@@ -108,7 +108,7 @@
                 <div>
                     <span class="h4" style="padding-top: 22px;">Company</span>
                     <ul>
-                        <li><a href="about.html">About us</a></li>
+                        <li><a href="/about.html">About us</a></li>
                     </ul>
                 </div>
                 <div>
@@ -121,8 +121,8 @@
                 <div>
                     <span class="h4" style="padding-top: 22px;">Follow Us</span>
                     <div class="flex" style="padding-top:20px;">
-                        <a target="_blank" href="https://instagram.com/trembit_life?utm_medium=copy_link"><picture><source srcset="view/images/instagram_icon.webp" type="image/webp"><img src="view/images/instagram_icon.png" width="38" height="38" alt="instagram" style="margin-right:25px;"></picture></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/trembit/"><picture><source srcset="view/images/link_in_logo.webp" type="image/webp"><img src="view/images/link_in_logo.png" width="38" height="38" alt="honor"></picture></a>
+                        <a target="_blank" href="https://instagram.com/trembit_life?utm_medium=copy_link"><picture><source srcset="{{asset('view/images/instagram_icon.webp')}}" type="image/webp"><img src="{{asset('view/images/instagram_icon.png')}}" width="38" height="38" alt="instagram" style="margin-right:25px;"></picture></a>
+                        <a target="_blank" href="https://www.linkedin.com/company/trembit/"><picture><source srcset="{{asset('view/images/link_in_logo.webp')}}" type="image/webp"><img src="{{asset('view/images/link_in_logo.png')}}" width="38" height="38" alt="honor"></picture></a>
                     </div>
                 </div>
             </div>
@@ -130,7 +130,7 @@
                 <div class="end">
                     <div class="flex column">
                         <script src="https://widget.clutch.co/static/js/widget.js?_v=20220716221822"></script> <div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="2" data-height="45" data-darkbg="1" data-clutchcompany-id="114502" style="width: 193px;"></div>
-                        <a target="_blank" href="https://www.red5pro.com/"><picture><source srcset="view/images/red5_pro_logo.webp" type="image/webp"><img src="view/images/red5_pro_logo.png" width="200" height="80" alt="red5"></picture></a>
+                        <a target="_blank" href="https://www.red5pro.com/"><picture><source srcset="{{asset('view/images/red5_pro_logo.webp')}}" type="image/webp"><img src="{{asset('view/images/red5_pro_logo.png')}}" width="200" height="80" alt="red5"></picture></a>
                     </div>
                 </div>
                 <div class="end">
