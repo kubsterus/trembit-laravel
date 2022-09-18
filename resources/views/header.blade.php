cfe0b5178b84506ecb273f6471aad45cb3be828a
Максим Куба <maxkuba@Mac-mini-Maksim.local>
2022-09-18 16:17:28 +0300

    header fix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,11 +2,11 @@
     <div class="wrapper headerWrapper">
         <div class="logo">
             <a href="https://trembit.com/">
-                <img alt="logo" id="whiteLogo" class="logoImage logo-link" src="view/images/logo@2x.svg" width="151" height="24">
-                <img alt="logo" id="blackLogo" class="logoImage logo-link" src="view/images/logoDark@2x.svg" width="151" height="24" style="display:none;">
+                <img alt="logo" id="whiteLogo" class="logoImage logo-link" src="{{asset('view/images/logo@2x.svg')}}" width="151" height="24">
+                <img alt="logo" id="blackLogo" class="logoImage logo-link" src="{{asset('view/images/logoDark@2x.svg')}}" width="151" height="24" style="display:none;">
             </a>
             <div class="stand-by">
-                <a href="https://standforukraine.com/"><picture><source srcset="view/images/fl_ua_n.webp" type="image/webp"><img alt="logo" src="view/images/fl_ua_n.png"></picture></a>
+                <a href="https://standforukraine.com/"><picture><source srcset="{{asset('view/images/fl_ua_n.webp')}}" type="image/webp"><img alt="logo" src="{{asset('view/images/fl_ua_n.png')}}"></picture></a>
                 <a href="https://standforukraine.com/"><span>Stand For Ukraine</span></a>
             </div>
         </div>
@@ -30,47 +30,47 @@
                         <g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)" /></g></g>
                     </svg>
                     <ul class="mob">
-                        <li><a href="web-development.html"><img alt="web" src="view/images/computer.svg" height="16" style="padding-right:12px;">Web development</a></li>
-                        <li><a href="mobile-development.html"><img alt="mobile development" src="view/images/software-development.svg" height="16" style="padding-right:17px;">Mob development</a></li>
-                        <li><a href="software-development.html"><img alt="sowtwar development" src="view/images/setting.svg" height="16" style="padding-right:15px;">Web Development kit</a></li>
-                        <li><a href="ui-ux.html"><img alt="art-and-design" src="view/images/art-and-design.svg" height="16" style="padding-right:15px;">UX/UI</a></li>
-                        <li><a href="qa.html"><img alt="qa" src="view/images/virus-warning(1).svg" height="16" style="padding-right:15px;">QA</a></li>
-                        <li><a href="rd.html"><img alt="rd" src="view/images/search(2).svg" height="16" style="padding-right:15px;">R&D </a></li>
+                        <li><a href="/web-development.html"><img alt="web" src="{{asset('view/images/computer.svg')}}" height="16" style="padding-right:12px;">Web development</a></li>
+                        <li><a href="/mobile-development.html"><img alt="mobile development" src="{{asset('view/images/software-development.svg')}}" height="16" style="padding-right:17px;">Mob development</a></li>
+                        <li><a href="/software-development.html"><img alt="sowtwar development" src="{{asset('view/images/setting.svg')}}" height="16" style="padding-right:15px;">Web Development kit</a></li>
+                        <li><a href="/ui-ux.html"><img alt="art-and-design" src="{{asset('view/images/art-and-design.svg')}}" height="16" style="padding-right:15px;">UX/UI</a></li>
+                        <li><a href="/qa.html"><img alt="qa" src="{{asset('view/images/virus-warning(1).svg')}}" height="16" style="padding-right:15px;">QA</a></li>
+                        <li><a href="/rd.html"><img alt="rd" src="{{asset('view/images/search(2).svg')}}" height="16" style="padding-right:15px;">R&D </a></li>
                     </ul>
                     <div class="drop-it">
                         <div class="wrapper headerWrapper">
                             <div class="dr-ul ind-r">
                                 <ul>
                                     <li>
-                                        <a href="web-development.html">
-                                            <img alt="computer" src="view/images/computer.svg" height="16" style="padding-right:15px;">Web development
+                                        <a href="/web-development.html">
+                                            <img alt="computer" src="{{asset('view/images/computer.svg')}}" height="16" style="padding-right:15px;">Web development
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="mobile-development.html">
-                                            <img alt="software development" src="view/images/software-development.svg" height="16" style="padding-right:21px;">Mob development
+                                        <a href="/mobile-development.html">
+                                            <img alt="software development" src="{{asset('view/images/software-development.svg')}}" height="16" style="padding-right:21px;">Mob development
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="software-development.html">
-                                            <img alt="setting" src="view/images/setting.svg" height="16" style="padding-right:19px;">Web Development kit
+                                        <a href="/software-development.html">
+                                            <img alt="setting" src="{{asset('view/images/setting.svg')}}" height="16" style="padding-right:19px;">Web Development kit
                                         </a>
                                     </li>
                                 </ul>
                                 <ul>
                                     <li>
-                                        <a href="ui-ux.html">
-                                            <img alt="art-and-design" src="view/images/art-and-design.svg" height="16" style="padding-right:15px;">UX/UI
+                                        <a href="/ui-ux.html">
+                                            <img alt="art-and-design" src="{{asset('view/images/art-and-design.svg')}}" height="16" style="padding-right:15px;">UX/UI
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="qa.html">
-                                            <img alt="qa" src="view/images/virus-warning(1).svg" height="16" style="padding-right:15px;">QA
+                                        <a href="/qa.html">
+                                            <img alt="qa" src="{{asset('view/images/virus-warning(1).svg')}}" height="16" style="padding-right:15px;">QA
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="rd.html">
-                                            <img alt="rd" src="view/images/search(2).svg" height="16" style="padding-right:15px;">R&D
+                                        <a href="/rd.html">
+                                            <img alt="rd" src="{{asset('view/images/search(2).svg')}}" height="16" style="padding-right:15px;">R&D
                                         </a>
                                     </li>
                                 </ul>
@@ -78,7 +78,7 @@
                         </div>
                     </div>
                 </li>
-                <li class="menu-item"><a class="menuItem" href="portfolio.html">Case studies</a></li>
+                <li class="menu-item"><a class="menuItem" href="/portfolio.html">Case studies</a></li>
                 <li class="menu-item drop-el drop-els3">
                     <a class="drop-r3">Industries</a>
                     <svg xmlns="http://www.w3.org/2000/svg" class="mob drop-tr drop-r3" width="22.276" height="12" viewBox="0 0 22.276 12">
@@ -92,11 +92,11 @@
                         <g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)" /></g></g>
                     </svg>
                     <ul class="mob">
-                        <li><a class="menuItem" href="/industries/webrtc-development.html"><img alt="webrtc development" src="view/images/computer.svg" height="16" style="padding-right:12px;">WebRTC</a></li>
-                        <li><a href="/industries/telemedicine-app-development.html"><img alt="telemedicine app development" src="view/images/software-development.svg" height="16" style="padding-right:17px;">Telemedicine App Development</a></li>
-                        <li><a href="/industries/fitness-app-development.html"><img alt="fitness app development" src="view/images/setting.svg" height="16" style="padding-right:15px;">Fitness App Development</a></li>
-                        <li><a href="/industries/dating-app-development.html"><img alt="dating app development" src="view/images/art-and-design.svg" height="16" style="padding-right:15px;">Dating App Development</a></li>
-                        <li><a href="/industries/video-chat-app-development.html"><img alt="video chat app" src="view/images/virus-warning(1).svg" height="16" style="padding-right:15px;">Video Chat App Development</a></li>
+                        <li><a class="menuItem" href="/industries/webrtc-development.html"><img alt="webrtc development" src="{{asset('view/images/computer.svg')}}" height="16" style="padding-right:12px;">WebRTC</a></li>
+                        <li><a href="/industries/telemedicine-app-development.html"><img alt="telemedicine app development" src="{{asset('view/images/software-development.svg')}}" height="16" style="padding-right:17px;">Telemedicine App Development</a></li>
+                        <li><a href="/industries/fitness-app-development.html"><img alt="fitness app development" src="{{asset('view/images/setting.svg')}}" height="16" style="padding-right:15px;">Fitness App Development</a></li>
+                        <li><a href="/industries/dating-app-development.html"><img alt="dating app development" src="{{asset('view/images/art-and-design.svg')}}" height="16" style="padding-right:15px;">Dating App Development</a></li>
+                        <li><a href="/industries/video-chat-app-development.html"><img alt="video chat app" src="{{asset('view/images/virus-warning(1).svg')}}" height="16" style="padding-right:15px;">Video Chat App Development</a></li>
                     </ul>
                     <div class="drop-it">
                         <div class="wrapper headerWrapper">
@@ -104,29 +104,29 @@
                                 <ul>
                                     <li>
                                         <a href="/industries/webrtc-development.html">
-                                            <img alt="webrtc" src="view/images/webrtc.svg" height="16" style="padding-right:15px;">WebRTC
+                                            <img alt="webrtc" src="{{asset('view/images/webrtc.svg')}}" height="16" style="padding-right:15px;">WebRTC
                                         </a>
                                     </li>
                                     <li>
                                         <a href="/industries/telemedicine-app-development.html">
-                                            <img alt="telemedicine app" src="view/images/telemedicine-app.svg" height="16" style="padding-right:21px;">Telemedicine App Development
+                                            <img alt="telemedicine app" src="{{asset('view/images/telemedicine-app.svg')}}" height="16" style="padding-right:21px;">Telemedicine App Development
                                         </a>
                                     </li>
                                     <li>
                                         <a href="/industries/fitness-app-development.html">
-                                            <img alt="fitness app" src="view/images/fitness-app.svg" height="16" style="padding-right:19px;">Fitness App Development
+                                            <img alt="fitness app" src="{{asset('view/images/fitness-app.svg')}}" height="16" style="padding-right:19px;">Fitness App Development
                                         </a>
                                     </li>
                                 </ul>
                                 <ul>
                                     <li>
                                         <a href="/industries/dating-app-development.html">
-                                            <img alt="datying app" src="view/images/datying-app.svg" height="16" style="padding-right:15px;">Dating App Development
+                                            <img alt="datying app" src="{{asset('view/images/datying-app.svg')}}" height="16" style="padding-right:15px;">Dating App Development
                                         </a>
                                     </li>
                                     <li>
                                         <a href="/industries/video-chat-app-development.html">
-                                            <img alt="video chat" src="view/images/video-chat.svg" height="16" style="padding-right:15px;">Video Chat App Development
+                                            <img alt="video chat" src="{{asset('view/images/video-chat.svg')}}" height="16" style="padding-right:15px;">Video Chat App Development
                                         </a>
                                     </li>
                                 </ul>
@@ -147,35 +147,35 @@
                         <g transform="translate(22.275 -102.296) rotate(90)"><g transform="translate(102.298 0)"><path class="a" d="M104.367,11.144l9.68-9.68A.857.857,0,0,0,112.835.251L102.548,10.537a.857.857,0,0,0,0,1.212l10.286,10.286a.857.857,0,0,0,1.212-1.212Z" transform="translate(114.296 22.275) rotate(180)" /></g></g>
                     </svg>
                     <ul class="mob">
-                        <li><a href="vatra.html"><picture><source srcset="view/images/LOGOVatra.webp" type="image/webp"><img alt="vatra" src="view/images/LOGOVatra.png" height="16" style="padding-right:12px;"></picture></a></li>
-                        <li><a href="stusan.html"><img alt="stusan" src="view/images/Stusanlogo.svg" height="16" style="padding-right:17px;"></a></li>
+                        <li><a href="/vatra.html"><picture><source srcset="{{asset('view/images/LOGOVatra.webp')}}" type="image/webp"><img alt="vatra" src="{{asset('view/images/LOGOVatra.png')}}" height="16" style="padding-right:12px;"></picture></a></li>
+                        <li><a href="/stusan.html"><img alt="stusan" src="{{asset('view/images/Stusanlogo.svg')}}" height="16" style="padding-right:17px;"></a></li>
                     </ul>
                     <div class="drop-it">
                         <div class="wrapper headerWrapper">
                             <div class="dr-ul">
                                 <ul class="sol-n">
                                     <li style="height: 58px;">
-                                        <a href="vatra.html" style="margin-bottom: 10px;">
-                                            <picture><source srcset="view/images/LOGOVatra.webp" type="image/webp"><img alt="LOGOVatra" src="view/images/LOGOVatra.png" height="39" style="padding-right:15px;"></picture>
+                                        <a href="/vatra.html" style="margin-bottom: 10px;">
+                                            <picture><source srcset="{{asset('view/images/LOGOVatra.webp')}}" type="image/webp"><img alt="LOGOVatra" src="{{asset('view/images/LOGOVatra.png')}}" height="39" style="padding-right:15px;"></picture>
                                         </a>
                                     </li>
-                                    <li style="width: 210px;"><a href="vatra.html" style=" font: normal normal normal 14px/19px Lato;">A White Label video conferencing solution suitable for large calls - up to 30 participants.</a></li>
+                                    <li style="width: 210px;"><a href="/vatra.html" style=" font: normal normal normal 14px/19px Lato;">A White Label video conferencing solution suitable for large calls - up to 30 participants.</a></li>
                                 </ul>
                                 <ul class="sol-n">
                                     <li>
-                                        <a href="stusan.html" style="margin-bottom: 10px;">
-                                            <picture><source srcset="view/images/LogoStusan.webp" type="image/webp"><img alt="LogoStusan" src="view/images/LogoStusan.png" height="39" style="padding-right:15px;"></picture>
+                                        <a href="/stusan.html" style="margin-bottom: 10px;">
+                                            <picture><source srcset="{{asset('view/images/LogoStusan.webp')}}" type="image/webp"><img alt="LogoStusan" src="{{asset('view/images/LogoStusan.png')}}" height="39" style="padding-right:15px;"></picture>
                                         </a>
                                     </li>
-                                    <li style="width: 210px;"><a href="stusan.html" style="font: normal normal normal 14px/19px Lato;">A White Label WebRTC P2P video conferencing solution. Suitable for 1 on 1 calls, and up to 5 participants.</a></li>
+                                    <li style="width: 210px;"><a href="/stusan.html" style="font: normal normal normal 14px/19px Lato;">A White Label WebRTC P2P video conferencing solution. Suitable for 1 on 1 calls, and up to 5 participants.</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </li>
-                <li class="menu-item"><a class="menuItem" href="about.html">About us</a></li>
+                <li class="menu-item"><a class="menuItem" href="/about.html">About us</a></li>
                 <li class="menu-item"><a class="menuItem" href="/blog/">Blog</a></li>
-                <li class="menu-item"><a class="menuItem" href="contacts.html">Contacts</a></li>
+                <li class="menu-item"><a class="menuItem" href="/contacts.html">Contacts</a></li>
             </ul>
         </nav>
 
