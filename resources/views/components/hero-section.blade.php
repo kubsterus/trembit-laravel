51e9d749375343646a9d32e805e1be695d282dc1
Byden <denisbidenko40@gmail.com>
2022-09-18 16:27:44 +0300

    recruting page header update to component

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,7 +1,6 @@
 @props([
     'title' => 'Page <br> Title',
     'image' => 'https://via.placeholder.com/300x200',
-    'text' => 'section text',
     'btn' => [
         'href' => 'https://google.com',
         'text' => 'Btn text'
@@ -12,14 +11,15 @@
         <div class="hero__inner">
             <div class="hero__title-wrp">
                 <h1 class="title hero__title">{!! $title !!}</h1>
-                <div class="bg-img-overlay"><picture><source srcset="{{$image}}" type="image/webp"><img src="{{$image}}" alt="Background"></source></picture></div>
+                <div class="bg-img-overlay hero__bg-mobile"><picture><source srcset="{{$image}}" type="image/webp"><img src="{{$image}}" alt="Background"></source></picture></div>
             </div>
             <div class="hero__desc-wrp">
-                <p class="hero__desc">
-                    {!! $text !!}
-                </p>
+                @if ($content ?? false)
+                    {{$content}} 
+                @endif
                 <a class="btn hero__btn" href="{{@$btn['href']}}">{{@$btn['text']}}</a>
             </div>
         </div>
     </div>
+    <div class="bg-img-overlay hero__bg-desktop"><picture><source srcset="{{$image}}" type="image/webp"><img src="{{$image}}" alt="Background"></source></picture></div>
 </section>
--- resources/views/pages/recruting.blade.php
+++ resources/views/pages/recruting.blade.php
@@ -4,7 +4,20 @@
 	@inlineCss(css/recruting.css)
 @endsection
 @section('content')
-	@include('sections.recruting.hero')
+	<x-hero-section
+		title="We are Trembit - IT company focused on developing video streaming solutions."	
+		:btn="[
+			'href' => '#',
+			'text' => 'Check our hot vacancies'
+		]"
+		:image="asset('img/hero/01.webp')"
+
+	>
+		<x-slot name="content">
+			<p class="hero__desc">Less micromanagement and bureaucracy.</p>
+			<p class="hero__desc">More responsibility and personal growth of team players.</p>
+		</x-slot>
+	</x-hero-section>
 	@include('sections.recruting.benefits')
 	@include('sections.recruting.skills')
 	@include('sections.recruting.vacancies')
